e82bb1d4c8ac5ad4807032a00187c0608e778b06
rcremadf <ricardocrema@gmail.com>
2025-01-17 13:17:41 -0300

    feat controller and dto

---
+++ src/user/user.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+
+@Controller('user')
+export class UserController {
+
+    @Get()
+    async getAllUsers() {
+        return JSON.stringify({ test: 'abcdef' });
+    }
+
+    @Post()
+    async createUser(@Body() createUser: CreateUserDto) {
+        return {
+            ...createUser,
+            password: undefined,
+        }
+
+    }
+
+}
